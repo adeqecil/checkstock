eb95a21076c6a30539e8aa80057562732ec0c006
adeqecil <ainur.rozikin@gmail.com>
2020-07-10 23:36:35 +0700

    authorz

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -21,7 +21,7 @@
                     <div class="alert alert-success">
                         {{ session('status') }}
                     </div>
-                    //
+                    
                 @endif
                 {{-- Add --}}
                 @if(Auth::user()->role=='admin')
